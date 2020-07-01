cb3b717968933b4d7d486bb03ec0d6dd201c8a5e
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-07-01 13:45:28 -0400

    In progress - player turn then dealer turn

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ class PlayerParent {
 //==================================================================================================
 // calculateHand() - Calculate the players hand by adding the value of each card.  Returns total.
 //==================================================================================================
-
     calculateHand() {
         let sum = 0;
         let holdAce = [];
@@ -69,7 +68,10 @@ class Player extends PlayerParent {
         this.bet = bet;
     }
 
-    isNaturalBlackJack() {
+//==================================================================================================
+// isNaturalBlackJack() - returns true if holding an ace and a 10 card
+//==================================================================================================
+isNaturalBlackJack() {
         if (this.hand.length > 2) {
             return false;
         } else {
@@ -83,8 +85,30 @@ class Player extends PlayerParent {
 }
 
 
+//==================================================================================================
+// Dealer - has name and hand. Can startHand(player).
+//==================================================================================================
+class Dealer extends PlayerParent {
 
+    startHand(player) {
+        player.hand.push(blackjack.nextCard());
+        player.hand.push(blackjack.nextCard());
+        console.log("Player hand: ", player.hand);
+        this.hand.push(blackjack.nextCard());
+        this.hand.push(blackjack.nextCard());
+        console.log("Dealer hand: ", this.hand);
+    }
+
+    hit(player){
+        player.hand.push(blackjack.nextCard());
+    }
+}
+
+
+
+//==================================================================================================
 // Card - holds name, value and imageFilename for later display of card image
+//==================================================================================================
 class Card {
     constructor (name, value, imageFilename){
         this.name = name;
@@ -92,7 +116,10 @@ class Card {
         this.imageFilename = imageFilename;
     }
 
-    isAce(){
+//==================================================================================================
+// isAce() - returns true if the first character of the name is "a"; indicates an ace
+//==================================================================================================
+isAce(){
         return this.name[0] == "a";
     }
 
@@ -101,31 +128,118 @@ class Card {
     }
 }
 
-
-
-let initializeDeckOfCards = () =>{
-    let deck = new Map();
-    let suit = ["c", "d", "h", "s"];
-    let faceCards = ["k", "q", "j"];
-    // create each suit
-    for (let i = 0; i < suit.length; i++) {
-        // create ace
-        let ace = new Card(`a${suit[i]}`, 11, `a${suit[i]}.jpg` );
-        deck.set(ace.name,ace);
-        // create face cards
-        for (let j = 0; j < faceCards.length; j++) {
-            let card = new Card(`${faceCards[j]}${suit[i]}`, 10, `${faceCards[j]}${suit[i]}.jpg`);
-            deck.set(card.name, card);
+class Blackjack {
+    constructor(){
+        this.deck = this.initializeDeckOfCards();
+        this.staticDeck = this.deck;
+        this.dealer = new Dealer("dealer",[]);
+        this.player = new Player("Stefani", 500, 100,[]);
+    }
+//==================================================================================================
+// initializeDeckOfCards() - creates the deck of cards in a Map
+//==================================================================================================
+    initializeDeckOfCards(){
+        let deck = new Map();
+        // deck = new Set();
+        let suit = ["c", "d", "h", "s"];
+        let faceCards = ["k", "q", "j"];
+        let cardId = 0; // Index for unique id for each card
+        // create each suit
+        for (let i = 0; i < suit.length; i++) {
+            // create ace
+            let ace = new Card(`a${suit[i]}`, 11, `a${suit[i]}.jpg` );
+            deck.set(cardId,ace);
+            cardId++;
+            // deck.add(ace);
+            // create face cards
+            for (let j = 0; j < faceCards.length; j++) {
+                let card = new Card(`${faceCards[j]}${suit[i]}`, 10, `${faceCards[j]}${suit[i]}.jpg`);
+                deck.set(cardId, card);
+                cardId++;
+                // deck.add(card);
+            }
+            // create number cards
+            for (let j = 2; j <= 10; j++) {
+                let card = new Card(`${j}${suit[i]}`, j, `${j}${suit[i]}.jpg`);
+                deck.set(cardId, card);   
+                cardId++;
+                // deck.add(card);                                 
+            }
         }
-        // create number cards
-        for (let j = 2; j <= 10; j++) {
-            let card = new Card(`${j}${suit[i]}`, j, `${j}${suit[i]}.jpg`);
-            deck.set(card.name, card);                                    
-        }               
-    } 
+        return deck;              
+    }
+
+//==================================================================================================
+// nextCard() - return next random card from deck and removes from deck
+//==================================================================================================
+    nextCard(){
+        let index = returnRandomNumber(0, 51);
+        let nextCard = this.deck.get(index);
+        let isRemoved = this.deck.delete(index);
+        return nextCard;
+    }
+
+    startGame(){
+
+
+        this.dealer.startHand(this.player);
+
+    
+
+        let playAgain = true;
+        while (playAgain) {
+
+            this.dealer.startHand(this.player);
+            
+            // let hitOrStay = prompt("Would you like to hit or stay?","h/s");
+            let hitOrStay = "h";
+            while (hitOrStay == "h"){
+                this.dealer.hit(this.player);
+                // hitOrStay = prompt("Would you like to hit or stay?","h/s");
+                hitOrStay = "s";
+            }
+            
+        //     Display player hand status
+            console.log("Player hand:  ", this.player.calculateHand());
+            if (this.player.isBust()){
+                console.log("Player has busted with ", this.player.calculateHand());
+            }
+            
+            while (this.dealer.calculateHand() < 17) {
+                this.dealer.hit(this.dealer);
+            }
+            console.log("Dealer hand:  ", this.dealer.calculateHand());
+            playAgain = false;
+
+        //     Compare hands.
+        //     let dealerTotal = dealer.calculateHand();
+        //     let playerTotal = player.calculateHand();
+
+        //     If dealerTotal > playerTotal {
+        //         player loses
+        //     } else if dealerTotal < playerTotal {
+        //         player wins
+        //         if (player.isNaturalBlackjack) {
+        //             player.wallet =+ player.bet * 2;
+        //         } else {
+        //             player.wallet =+ player.bet * 1.5;
+        //         }
+        //     } else {
+        //         // It's a push.
+        //         return bet to player wallet
+        //     }
+        //     Ask player if they want to play again
+        }
+
+        }
+
+    
 }
 
-initializeDeckOfCards();
+
+
+const blackjack = new Blackjack();
+blackjack.startGame();
 
 let hand = [
     new Card("2c", 2),
